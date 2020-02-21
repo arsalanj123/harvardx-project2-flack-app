7b1b785ed28b451d390d3137cb64c3b2ce6e12f0
Arsalan Jawaid <arsalan.jawaidstu@gmail.com>
2020-02-21 20:05:10 +0400

    Backend - Add login route and logic of the page in backend

--- application.py
+++ application.py
@@ -1,13 +1,52 @@
 import os
+from flask import Flask, request, session, render_template
+from flask_session import Session
+#from flask_socketio import emit, SocketIO
 
-from flask import Flask
-from flask_socketio import SocketIO, emit
-
+#Flask Application config
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
+app.debug = Flask
+
+#Socket
+#socketio = SocketIO(app)
+
+#Session configuration
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = 'filesystem'
+Session(app)
 
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
-    return "Project 2: TODO"
+    #session check
+    session["session_id"] = 0
+    print(session["session_id"])
+
+    if request.method == "GET":
+    #if session already in work
+        if session["session_id"] > 0:
+            return render_template("hompage.html")
+    #if session not present 
+        else:
+            return render_template("login.html")
+
+
+    elif request.method == "POST":
+        if session["session_id"] > 0:
+    #if session already in work
+            return render_template("hompage.html")
+    #if session not present, signup
+    # Add user   
+        else:
+            return render_template("hompage.html")
+
+
+@app.route("/navbar", methods=["GET","POST"])
+def navbar():
+    return render_template("navbar.html")
+
+
+@app.route("/test", methods=["GET","POST"])
+def test():
+    return render_template("test.html")
