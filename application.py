47af1f120e2070e2cf2e2182dfd143786e0486c8
Arsalan Jawaid <arsalan.jawaidstu@gmail.com>
2020-03-05 17:29:44 +0400

    working sessions but session-security not implemented yet

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import Flask, request, session, render_template, redirect
 from flask_session import Session
 from flask_socketio import SocketIO, emit
 from datetime import datetime
+from random import randint
 
 
 # Flask Application config
@@ -39,7 +40,9 @@ def index():
     # session check
 
     session["session_id"] = 0
+    session["username"] = ""
     print(session["session_id"])
+    print(session["username"])
 
     if request.method == "GET":
         # if user already loggedin
@@ -47,6 +50,8 @@ def index():
             return redirect("/main")
     # if user not loggedin
         else:
+            print(session["session_id"])
+            print(session["username"])
             return render_template("login.html")
 
     # if post request
@@ -54,6 +59,8 @@ def index():
 
         # if user already loggedin
         if session["session_id"] > 0:
+            print(session["session_id"])
+            print(session["username"])
             return redirect("/main")
     # when user added username and press submit button
         else:
@@ -62,21 +69,26 @@ def index():
                 return render_template("login.html", javascript_alert="Username Already exists! Please choose another one!")
             else:                
                 session["username"] = username
+                session["session_id"] = randint(1, 99)
                 all_users.append(username)
-                print(username)
+                print(session["session_id"])
+                print(session["username"])
                 return redirect("/main")
 
 
 # default page
 @app.route("/main", methods=["GET", "POST"])
-def main():                
+def main():      
+    print(session["session_id"])
+    print(session["username"])          
     larger = find_larger(all_users, all_channels)
     print(all_users)
     return render_template("homepage.html", all_users=all_users, all_channels=all_channels, larger=larger)
 
 @app.route("/channel_create", methods=["POST"])
 def channel_create():
-    
+    print(session["session_id"])
+    print(session["username"])
     new_channel_to_create = request.form.get("new_channel_from_form")
     
     if new_channel_to_create in all_channels:
@@ -92,6 +104,8 @@ def channel_create():
 # channel page
 @app.route("/channel", methods=["GET", "POST"])
 def channel():
+    print(session["session_id"])
+    print(session["username"])
   #  print(session["username"])
     #    if session["username"] != "":
     return render_template("channel.html")
@@ -101,10 +115,14 @@ def channel():
 
 @app.route("/channels/<string:channel>", methods=['GET', 'POST'])
 def channels(channel):
+    print(session["session_id"])
+    print(session["username"])
     return render_template("channel.html", channel=channel)
 
 @app.route("/people/<string:person>", methods=['GET', 'POST'])
 def people(person):
+    print(session["session_id"])
+    print(session["username"])
     return render_template("people.html", person=person)
 
 
@@ -123,12 +141,14 @@ def vote(data):
     emit("announce vote", {"selection": selection,
                            "username": username,
                            "time": message_time}, broadcast=True)
-
+    Messages_all_channels = {}
 
 
 @app.route("/logout", methods=["GET", "POST"])
 def logout():
+    print("logging out")
     print(session["session_id"])
+    print(session["username"])
     session["session_id"] = 0
     session["username"] = ""
     return redirect("/login")
